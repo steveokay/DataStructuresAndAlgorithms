790e7cc1e5a8dbb79e7f41d96178fda20b1f403c
Steve Okoth <Steve.Okoth@ncbagroup.com>
2021-02-16 17:03:58 +0300

    Singly Linked List implementation

---
+++ LinkedList/src/SinglyLinkedList/LinkedList.java
@@ -0,0 +1,138 @@
+package SinglyLinkedList;
+
+public class LinkedList {
+
+    // print the linked list
+    static void printList(Node head){
+        Node current = head;
+
+        while(current != null){
+            System.out.print(current.data + " => ");
+            current = current.next;
+        }
+        System.out.print("NULL\n");
+    }
+
+    // check if value exists in the given list
+    static boolean checkValueExists(Node head, int val){
+
+        // init current pointer
+        Node current = head;
+
+        // if head is null return false
+        if(head == null){
+            return false;
+        }
+
+        while(current != null){
+            if(current.data == val){
+                return true;
+            }
+            // move pointer to next array location
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    // function to insert the value at the end of the linked list
+    static void insert(Node head, int value){
+
+        Node current = head;
+
+        // init the node to insert
+        Node newNode = new Node(value);
+
+        if(current.next != null)
+            insert(current.next,value);
+        else
+            current.next = newNode;
+    }
+
+    // insert value after a given linked list value
+    // assumption is that you want to insert after the 1st occurrence
+    // in case a number occurs more than once we will just insert after the 1st occurrence
+    static Node insertAfter(Node head, int previous, int value){
+
+        Node newNode = new Node(value);
+        Node current = head;
+
+        if(current == null){
+            System.out.println("Previous entered doesn't exist in the linked list");
+            return null;
+        }
+
+        if( current.data != previous){
+            insertAfter(current.next, previous, value);
+        }else {
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+
+        return head;
+    }
+
+    // function to implement deep copy of the linked list
+    static Node deepCopy(Node head){
+
+        // if null then return null
+        if(head == null){
+            return null;
+        }
+
+        Node nextNode = head.next;
+        Node current = head;
+
+        // new deep copy node
+        Node newNode = new Node(current.data);
+
+        if(current != null){
+            newNode.next = nextNode;
+            deepCopy(current.next);
+
+        }else{
+            return null;
+        }
+
+        return newNode;
+    }
+
+    // removes value from linked list
+    // removes the first occurrence of value provided
+    static void delete(Node head, int val){
+
+        if(head == null)
+            return;
+
+        Node current = head;
+        Node previous = null;
+
+        // we use iterative instead of recursive
+        while(current != null){
+            // check if data is the one to delete
+            if(current.data == val){
+                // delete
+                // take cur.previous node and point it to cur.next
+                previous.next = current.next;
+                return;
+            }
+
+            previous = current;
+            current = current.next;
+        }
+
+    }
+
+    static Node sortAsc(Node head){
+        return null;
+    }
+
+    static Node sortDesc(Node head){
+        return null;
+    }
+
+    static Node reverseList(Node head){
+        return null;
+    }
+
+}
---
+++ LinkedList/src/SinglyLinkedList/Main.java
@@ -0,0 +1,46 @@
+package SinglyLinkedList;
+
+public class Main {
+    public static void main(String[] args){
+
+        // Init nodes
+        Node list1 = new Node(1);
+        Node list2 = new Node(2);
+        Node list3 = new Node(3);
+        Node list4 = new Node(4);
+        Node list5 = new Node(5);
+        Node list6 = new Node(6);
+
+        // init pointers
+        list1.next = list2;
+        list2.next = list3;
+        list3.next = list4;
+        list4.next = list5;
+        list5.next = list6;
+
+        // check if value exists in linked list
+        System.out.println(LinkedList.checkValueExists(list1, 2));
+        LinkedList.printList(list1);
+
+        // insert val
+        LinkedList.insert(list1,7);
+        LinkedList.printList(list1);
+
+        // insert after a given node value
+        LinkedList.insertAfter(list1,3,8);
+        LinkedList.printList(list1);
+
+        // implement deep copy
+        LinkedList.insertAfter(list1,7,9);
+        System.out.println( "before >> " + list1);
+        Node ret = LinkedList.deepCopy(list1);
+        System.out.println( "after >> " + ret);
+        LinkedList.printList(ret);
+
+        // delete node
+        System.out.println("Deleting node");
+        LinkedList.delete(list1,7 );
+        LinkedList.printList(ret);
+
+    }
+}
