26c322433a01c8776ee3605b174d64923bc16f3b
Steve Okoth <Steve.Okoth@ncbagroup.com>
2021-02-17 12:20:08 +0300

    creating binary tree file structure

---
+++ LinkedList/src/BinaryTree/Main.java
@@ -0,0 +1,6 @@
+package BinaryTree;
+
+public class Main {
+    public static void main(String[] args){
+    }
+}
---
+++ LinkedList/src/BinaryTree/Node.java
@@ -0,0 +1,12 @@
+package BinaryTree;
+
+public class Node {
+
+    int data;
+    Node left, right;
+
+    Node(int value){
+        data = value;
+        left = right = null;
+    }
+}
