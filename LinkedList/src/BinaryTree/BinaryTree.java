5d4650ca1e0db86550d407930a5e95d55ca353ab
Steve Okoth <mokaysteve@gmail.com>
2021-02-17 14:27:53 +0300

    implement dfs : inorder traversal

--- LinkedList/src/BinaryTree/BinaryTree.java
+++ LinkedList/src/BinaryTree/BinaryTree.java
@@ -1,4 +1,39 @@
 package BinaryTree;
 
 public class BinaryTree {
+
+    // dfs : f(x) to perform inorder traversal
+    public void inorder(Node head) {
+        if(head == null)
+            return;
+
+        // recur left
+        inorder(head.left);
+
+        // print node
+        System.out.print(head.data + " ");
+
+        // recur right
+        inorder(head.right);
+    }
+
+    // dfs function to do a preorder traversal on the tree
+    public void preorder(Node head) {
+    }
+
+    // dfs : f(X) to do a postorder traversal on the tree
+    public void postorder(Node head) {
+
+    }
+
+    // function  to print node values
+    public void printNodes(Node head) {
+
+    }
+
+    // function to count all nodes in the binary tree
+    public void countNodes(Node head) {
+
+    }
+
 }
--- LinkedList/src/BinaryTree/Main.java
+++ LinkedList/src/BinaryTree/Main.java
@@ -2,5 +2,16 @@ package BinaryTree;
 
 public class Main {
     public static void main(String[] args){
+
+        Node tree = new Node(1);
+        tree.left = new Node(2);
+        tree.right = new Node(3);
+        tree.left.left = new Node(4);
+        tree.left.right = new Node(5);
+        tree.right.left = new Node(6);
+
+        // perform
+        BinaryTree binary = new BinaryTree();
+        binary.inorder(tree);
     }
 }
