938799040f54fe6d6a3452e60a6ff5a9cff31631
Steve Okoth <mokaysteve@gmail.com>
2021-02-22 13:26:46 +0300

    DoublyLinkedList : creating structure for impl of doubly linked lists

---
+++ LinkedList/src/DoublyLinkedList/Node.java
@@ -0,0 +1,14 @@
+package DoublyLinkedList;
+
+public class Node {
+
+    int data;
+    Node prev;
+    Node next;
+
+    Node(int value){
+        data = value;
+        prev = null;
+        next = null;
+    }
+}
