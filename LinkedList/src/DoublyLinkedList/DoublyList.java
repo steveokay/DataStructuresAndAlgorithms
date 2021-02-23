0f31daa49fe1df0b7466614d71b7985d38f9e7e7
Steve Okoth <mokaysteve@gmail.com>
2021-02-23 10:22:47 +0300

    dll : add function to print all next nodes in a dll starting from first node

--- LinkedList/src/DoublyLinkedList/DoublyList.java
+++ LinkedList/src/DoublyLinkedList/DoublyList.java
@@ -1,4 +1,18 @@
 package DoublyLinkedList;
 
 public class DoublyList {
+
+    void printAllNextNodes(Node dll){
+
+        Node current = dll;
+
+        if(current == null){
+            System.out.print("NULL");
+            return;
+        }
+
+        System.out.print(current.data + " => ");
+
+        printAllNextNodes(current.next);
+    }
 }
--- LinkedList/src/DoublyLinkedList/Main.java
+++ LinkedList/src/DoublyLinkedList/Main.java
@@ -1,4 +1,35 @@
 package DoublyLinkedList;
 
 public class Main {
+
+    public static void main(String[] args){
+
+        // init nodes
+        Node dll1 = new Node(1);
+        Node dll2 = new Node(2);
+        Node dll3 = new Node(3);
+        Node dll4 = new Node(4);
+        Node dll5 = new Node(5);
+        Node dll6 = new Node(6);
+
+        // next nodes
+        dll1.next = dll2;
+        dll2.next = dll3;
+        dll3.next = dll4;
+        dll4.next = dll5;
+        dll5.next = dll6;
+
+        // prev nodes
+        dll2.prev = dll1;
+        dll3.prev = dll2;
+        dll4.prev = dll3;
+        dll5.prev = dll4;
+        dll6.prev = dll5;
+
+        DoublyList doubly = new DoublyList();
+
+        System.out.println("Print all next nodes");
+        doubly.printAllNextNodes(dll1);
+
+    }
 }
